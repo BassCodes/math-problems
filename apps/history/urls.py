0282a1076370ac945d45c52b55396f9102076a8d
Alexander Bass <alexander@alexanderbass.com>
2025-05-22 16:26:42 -0400

    add basic problem history timeline

--- apps/history/urls.py
+++ apps/history/urls.py
@@ -3,7 +3,5 @@ from django.urls import path
 from .views import ProblemHistoryView
 
 urlpatterns = [
-    path(
-        "problems/<int:pk>", ProblemHistoryView.as_view(), name="problem_history_detail"
-    ),
+    path("problems/<int:pk>", ProblemHistoryView.as_view(), name="problem_history"),
 ]
--- apps/history/views.py
+++ apps/history/views.py
@@ -1,29 +1,39 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import TemplateView
 
 from problems.models import Problem
 
 
-class ProblemHistoryView(DetailView):
-    model = Problem
+class ProblemHistoryView(TemplateView):
     template_name = "history/problem_history.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        problem = context["problem"]
+
+        history_queryset = None
+        if "problem" in context:
+            history_queryset = context["problem"].history.all().order_by("history_date")
+        else:
+            context["problem"] = Problem.history.filter(id=context["pk"]).first()
+            history_queryset = Problem.history.filter(id=context["pk"]).order_by(
+                "history_date"
+            )
 
         diffs = []
         last_hist = None
-        for hist in problem.history.all():
-            if last_hist is None:
-                diffs.append({"history": hist, "diff": None})
-            else:
-                diffs.append(
-                    {
-                        "history": hist,
-                        "diff": last_hist.diff_against(hist).changed_fields,
-                    }
-                )
+        for hist in history_queryset:
+            diff_value = None
+            if last_hist:
+                diff_value = last_hist.diff_against(hist)
+                if not diff_value.changed_fields:
+                    diff_value = None
+
+            diffs.append(
+                {
+                    "history": hist,
+                    "diff": diff_value,
+                }
+            )
             last_hist = hist
 
         context["diffs"] = diffs
